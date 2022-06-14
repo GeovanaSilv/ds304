b888bd899644d6e3f18d4781073a17b313492ddc
GEOVANA OLIVEIRA BARROS DA SILVA <u21430@LAPA17.academico.cotuca.unicamp.br>
2022-06-14 18:12:32 -0300

    kk

--- main.js
+++ main.js
@@ -8,14 +8,28 @@ app.use (express.json());
 app.use(express.json({type:'application/vnd.api+json'}));
 app.use(cors());
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3002;
 
 
-
+//exemplo do uso do get 
 app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+ '/index.html'));
+});
 
-res.sendFile(path.join(__dirname+'/index.html'));
 
+
+//POST -> serve para incluir dados no servidor 
+app.post('/api',(req,res)=>{
+    console.log('Foi  mandado:\n ',req.body);
+    _key = req.body.key;
+    //_key2 = req.
+ 
+    res.status(200).send(
+    {
+        sucess:'true',
+        message: 'Ok correto ...'
+    }
+    );
 });
 
 app.listen(port,() => console.log(`EStou escutando na porta ${port}`))
